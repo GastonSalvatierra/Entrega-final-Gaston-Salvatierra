f5b26e9c38d4426edd7884d2445b48d5826cf1e4
Gaston Salvatierra <gastonsalvatierra@icloud.com>
2023-01-04 21:49:13 -0300

    ¨Pre-entrega1-Salvatierra¨

---
+++ src/Components/CartWidget.js
@@ -0,0 +1,11 @@
+import carrito from '../assets/carrito-de-compras.png';
+
+const CartWidget = () => {
+  return (
+        <div className="compra">
+            <a><img className="carrito" src={carrito}/><span className="contador">0</span></a>
+        </div>
+  )
+}
+
+export default CartWidget;
